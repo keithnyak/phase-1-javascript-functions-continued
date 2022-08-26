1a075ef1d1e2bcba103264e9c8d2a102b8959e16
keith <nyakinyekeith@gmail.com>
2022-08-26 20:52:58 +0300

    complomplete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function saturdayFun(activity="roller-skate",time="late"){
+    console.log(`This Saturday,I want to ${activity} till ${time} `)
+
+}
+saturdayFun();
 // code your solution here
+
+//lab2
+const mondayWork=function(){
+    console.log("This Monday, I will ${activity}")
+
+}
+mondayWork("work from home")
+
+
